708a7b08a955595a4356788d506a829738e3268b
Jonah Broyer <broyerj@oregonstate.edu>
2020-10-30 10:40:14 -0700

    Completed Mult program

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,31 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Computes RAM[2]=RAM[0]+RAM[0]+...+RAM[0] {RAM[1] times}
+// R2 = 0
+// i = 1
+// while (i <= R1):
+// 	R2 = R0 + R0
+// 	i = i + 1
+	@i
+	M=1 // i=1
+	@R2
+	M=0 // R2=0
+(LOOP)
+	@i
+	D=M
+	@R1
+	D=D-M
+	@END
+	D;JGT
+	@R0
+	D=M 	
+	@R2
+	M=D+M // R0 + R0
+	@i
+	M=M+1 // i++
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
